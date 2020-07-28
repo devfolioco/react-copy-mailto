838e935379749c866d2cb67055cd37b8d3fe655f
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-28 19:27:04 +0530

    :wrench: Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
                 enforce: "pre",
