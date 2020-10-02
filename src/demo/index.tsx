322938d1c30fa1c4b4ecae12bcc5869ae8b3abfe
Kumar Shashank <1711shashank@gmail.com>
2020-10-02 19:22:47 +0530

    updated interactive UI

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,14 +1,21 @@
+import { Button, colors } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import CopyMailTo from "../lib";
+import "./app.css";
 
 const App = () => (
-  <div
-    style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
-  >
-    <h1 style={{ marginBottom: "50px" }}>Copy email address to clipboard</h1>
-    <CopyMailTo email="email@domain.com" />
+  <div className="app">
+    <div className="app__body">
+      <img
+        src="https://www.flaticon.com/svg/static/icons/svg/926/926768.svg"
+        alt=""
+      />
+      <h1>Copy email address to clipboard</h1>
+      <div className="app__button">
+        <CopyMailTo email="email@domain.com" />
+      </div>
+    </div>
   </div>
 );
 
