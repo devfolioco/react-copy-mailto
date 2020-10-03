c571f6789bb47a31cf8b84eceb25b8c097e7077c
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-03 23:33:14 +0530

    enhanced UI

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -8,7 +8,19 @@ const App = () => (
     style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
   >
     <h1 style={{ marginBottom: "50px" }}>Copy email address to clipboard</h1>
-    <CopyMailTo email="email@domain.com" />
+    <h3>Click on the Email to copy it to the clipboard</h3>
+
+    <div
+    style={{
+      marginTop: '10%',
+      backgroundColor: 'white',
+      padding: '80px'
+    }}
+    >
+      <CopyMailTo email="email@domain.com" /><br/><br/>
+      <CopyMailTo email="devfolio@dev.co.in" /><br/><br/>
+      <CopyMailTo email="helloworld@hello.com" />
+    </div>
   </div>
 );
 
