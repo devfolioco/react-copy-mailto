22a1be0f23c5d18d427bf76314958b2cc2908f38
AJAYK-01 <ajayk@cet.ac.in>
2020-10-21 16:14:11 +0530

    add to demo dark/light mode and email input customisation

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import CopyMailTo from "../lib";
 
-const App = () => (
+function App () {
+  
+  type theme = "dark" | "light" ;
+  const [myTheme, setTheme] = useState<theme>('dark');
+  const [email, setEmail] = useState('email@domain.com');
+
+  return (
   <div
     style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
   >
+    <div>
+      <button onClick={() => { myTheme=='dark'?setTheme('light'):setTheme('dark') }}>
+      { 'Click for ' + (myTheme=='dark'?'Light':'Dark') + ' tooltip style' } 
+      </button>
+      <label style={{paddingLeft: '30px' }}>Email:</label>
+      <input type="text" id="email" name="email" value={email} onChange={(x) => setEmail(x.target.value)}>
+      </input>
+    </div>
+
     <h1 style={{ marginBottom: "50px" }}>Copy email address to clipboard</h1>
-    <CopyMailTo email="email@domain.com" />
+    <CopyMailTo email={email} theme={myTheme} />
   </div>
-);
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
